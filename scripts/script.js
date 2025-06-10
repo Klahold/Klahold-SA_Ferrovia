49b55685da2b07adde31bf467f8c053c30aa93a5
kaua <kaua_r_ribeiro@estudante.sesisenai.org.br>
2025-06-10 12:00:11 -0300

    ADD - relatorio completo

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,18 @@
-function teste() {
-    console.log(aaaaaaaa);
+
+let i = false
+
+function fun(){
+    
+
+    if(i == false){
+    
+    document.getElementById("frame1").style.visibility="visible";
+    document.getElementById("frame1").classList.add("style");
+        i=true
+    }else{
+        document.getElementById("frame1").style.visibility="hidden";
+        document.getElementById("frame1").classList.add("style");
+        i=false
+    }
 }
 
-teste()
