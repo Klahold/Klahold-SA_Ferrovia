6ae5cfac2d219b64d81edef365dc6d4957ca6393
Gustavo <gustavo_klahold@estudante.sesisenai.org>
2025-09-11 09:39:10 -0300

    ADD - db.php

---
+++ config/db.php
@@ -0,0 +1,13 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "hermes_enterprise";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Conexao falhou: " . $conn->connect_error);
+}
+?>
